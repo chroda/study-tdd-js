7c096bbf44cb73283d5bbd58a673cbc63e3e51f8
chroda <chroda@chroda.com.br>
2018-08-15 20:52:48 -0300

    calc

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -3,55 +3,55 @@ let calc = require('../src/main.js');
 
 describe('Calc', () => {
   describe('Smoke tests', () => {
-    it('should exists the calc lib',() => {
+    it('should exists the calc lib', () => {
       expect(calc).to.exist;
     });
 
-    it('should exists the method `sum`',() => {
+    it('should exists the method `sum`', () => {
       expect(calc.sum).to.exist;
       expect(calc.sum).to.be.an.function;
     });
 
-    it('should exists the method `sub`',() => {
+    it('should exists the method `sub`', () => {
       expect(calc.sub).to.exist;
       expect(calc.sub).to.be.an.function;
     });
 
-    it('should exists the method `mult`',() => {
+    it('should exists the method `mult`', () => {
       expect(calc.mult).to.exist;
       expect(calc.mult).to.be.an.function;
     });
 
-    it('should exists the method `div`',() => {
+    it('should exists the method `div`', () => {
       expect(calc.div).to.exist;
       expect(calc.div).to.be.an.function;
     });
   });
 
   describe('Sum', () => {
-    it('should return 4 when `sum(2,2)`',() => {
+    it('should return 4 when `sum(2,2)`', () => {
       expect(calc.sum(2,2)).to.be.equal(4);
     });
   });
 
   describe('Sub', () => {
-    it('should return 4 when `sub(6,2)`',() => {
+    it('should return 4 when `sub(6,2)`', () => {
       expect(calc.sub(6,2)).to.be.equal(4);
     });
 
-    it('should return -4 when `sub(6,10)`',() => {
+    it('should return -4 when `sub(6,10)`', () => {
       expect(calc.sub(6,2)).to.be.equal(4);
     });
   });
 
   describe('Mult', () => {
-    it('should return 4 when `mult(2,2)`',() => {
+    it('should return 4 when `mult(2,2)`', () => {
       expect(calc.mult(2,2)).to.be.equal(4);
     });
   });
 
   describe('Div', () => {
-    it('should return 4 when `div(4,2)`',() => {
+    it('should return 4 when `div(4,2)`', () => {
       expect(calc.div(4,2)).to.be.equal(2);
     });
   });
