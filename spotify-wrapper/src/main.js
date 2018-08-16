48532458555f035cd86a26eab1e21548aeccaa6e
chroda <chroda@chroda.com.br>
2018-08-16 19:31:12 -0300

    so far

--- spotify-wrapper/src/main.js
+++ spotify-wrapper/src/main.js
@@ -1,5 +1,5 @@
-export const search = () => {}
-export const searchArtists = () => {}
-export const searchAlbums = () => {}
-export const searchTracks = () => {}
-export const searchPlaylists = () => {}
+export const search = () => {};
+export const searchArtists = () => {};
+export const searchAlbums = () => {};
+export const searchTracks = () => {};
+export const searchPlaylists = () => {};
